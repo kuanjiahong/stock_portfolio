a85571ef4bed23ca3b86480398cfd8e0dc4b380a
Jia Hong <kuanjiahong@gmail.com>
2022-07-30 22:26:29 +0800

    Added Portfolio model

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+from portfolio_app.custom_validators import validate_positive_number
+
+
+class Portfolio(models.Model):
+    stock_symbol = models.CharField(max_length=200, unique=True)
+    purchased_price = models.DecimalField(max_digits=6, decimal_places=6, validators=[validate_positive_number])
+    quantity = models.IntegerField(validators=[validate_positive_number])
+    purchased_timestamp = models.DateTimeField('purchased_timestamp')
