4860815401bf2ff495200a3765144217da68394e
Jia Hong <kuanjiahong@gmail.com>
2022-07-30 22:24:58 +0800

    Added url configuration

--- stock_portfolio/urls.py
+++ stock_portfolio/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from portfolio_app import views
 
 urlpatterns = [
+    path('portfolio_app/', include('portfolio_app.urls')),
     path('admin/', admin.site.urls),
 ]
