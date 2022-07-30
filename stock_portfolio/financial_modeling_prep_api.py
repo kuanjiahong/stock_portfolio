c331984137fa8e3c9d6ed4e0cce9d3a8524d33b2
Jia Hong <kuanjiahong@gmail.com>
2022-07-30 15:29:23 +0800

    Added buy stock function

--- stock_portfolio/financial_modeling_prep_api.py
+++ stock_portfolio/financial_modeling_prep_api.py
@@ -1,8 +1,28 @@
 """A module to interact with Financial Modeling Prep API"""
 
-from dotenv import load_dotenv
 import os
+
+from dotenv import load_dotenv
+import requests
 
 load_dotenv()
 
 API_KEY = os.getenv("FINANCE_MODELING_PREP_API_KEY")
+API_ADDRESS = "https://financialmodelingprep.com/api/v3/"
+
+
+def buy_stock(stock_symbol: str) -> object:
+    """
+    Buy stock
+
+    :param stock_symbol: the stock symbol to buy e.g. AAPL
+    :return: {"symbol": str, "price": number, "volume": number}
+    """
+
+    url = API_ADDRESS + "quote-short/" + stock_symbol + "?apikey=" + API_KEY
+
+    return requests.request("GET", url).json()
+
+
+if __name__ == "__main__":
+    print(buy_stock("AAPL"))
