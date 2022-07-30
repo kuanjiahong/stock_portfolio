6a4e7a4ea5dc799534adb3a8b6a890b126e95e88
Jia Hong <kuanjiahong@gmail.com>
2022-07-30 22:26:01 +0800

    Added home page endpoint

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+
+def index(request: HttpRequest):
+    """Home page of portfolio app"""
+
+    return HttpResponse("Hello, world. You're at the portfolio index.")
