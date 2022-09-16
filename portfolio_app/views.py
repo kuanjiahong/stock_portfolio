76ba096995f96d99c77430577e52783cbbe1fdee
Jia Hong <kuanjiahong@gmail.com>
2022-09-16 13:53:01 +0800

    Added function to retrieved data from Google Sheets

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -1,29 +1,9 @@
-from decimal import Decimal
-
 from django.http import HttpRequest
 from django.shortcuts import render
-from django.utils import timezone
-
-from stock_portfolio.financial_modeling_prep_api import get_stock_price
-from .models import Portfolio
-
+from helpers.google_api_helper.read_sheets import get_values, create_dict_from_value_range, SPREADSHEET_ID, RANGE_NAME
 
 def index(request: HttpRequest):
     """Home page of portfolio app"""
-
-    today_utc = timezone.now()
-    today_isoweekday = today_utc.isoweekday()
-    # Monday: 1 - Sunday: 7
-    if today_isoweekday == 7:
-        stock_price = str(get_stock_price("AAPL")[0]['price'])
-        stock_price = Decimal(stock_price)
-        total_cost = Decimal(str(stock_price * 1000))
-        portfolio = Portfolio(stock_symbol="AAPL", purchased_price=Decimal("162.51"), quantity=1000,
-                                  purchased_timestamp=today_utc, total_cost=total_cost)
-        portfolio.save()
-
-    latest_transactions_list = Portfolio.objects.order_by('-purchased_timestamp')
-
+    latest_transactions_list = create_dict_from_value_range(get_values(SPREADSHEET_ID, RANGE_NAME))
     context = {'latest_transactions_list': latest_transactions_list}
-
     return render(request, 'portfolio_app/index.html', context)
