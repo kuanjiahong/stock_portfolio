7bafa8352847fa61c43bbaa4f37f909b0c984a5f
Jia Hong <kuanjiahong@gmail.com>
2022-09-16 14:13:50 +0800

    Added line break

--- portfolio_app/views.py
+++ portfolio_app/views.py
@@ -2,6 +2,7 @@ from django.http import HttpRequest
 from django.shortcuts import render
 from helpers.google_api_helper.read_sheets import get_values, create_dict_from_value_range, SPREADSHEET_ID, RANGE_NAME
 
+
 def index(request: HttpRequest):
     """Home page of portfolio app"""
     latest_transactions_list = create_dict_from_value_range(get_values(SPREADSHEET_ID, RANGE_NAME))
