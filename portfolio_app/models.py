78e8fb9dcae3b7f8c21341a5383a4ede6bd52c6a
Jia Hong <kuanjiahong@gmail.com>
2022-09-16 12:26:43 +0800

    Removed unique

--- portfolio_app/models.py
+++ portfolio_app/models.py
@@ -4,7 +4,7 @@ from portfolio_app.custom_validators import validate_positive_number
 
 
 class Portfolio(models.Model):
-    stock_symbol = models.CharField(max_length=200, unique=True)
+    stock_symbol = models.CharField(max_length=200)
     purchased_price = models.DecimalField(max_digits=19, decimal_places=6, validators=[validate_positive_number])
     quantity = models.IntegerField(validators=[validate_positive_number])
     purchased_timestamp = models.DateTimeField('purchased_timestamp')
