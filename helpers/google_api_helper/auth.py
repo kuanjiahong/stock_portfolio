d69ee7ff6a490f61da444e5ede9342054158d931
Jia Hong <kuanjiahong@gmail.com>
2022-09-16 13:52:06 +0800

    Changed credentials.json to google_api_credentials.json and updated the file path

--- helpers/google_api_helper/auth.py
+++ helpers/google_api_helper/auth.py
@@ -6,6 +6,7 @@ from google.oauth2 import service_account
 
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 
-credentials = service_account.Credentials.from_service_account_file('../../google_api_credentials.json', scopes=SCOPES)
+credentials = service_account.Credentials.from_service_account_file(
+    'helpers/google_api_helper/google_api_credentials.json', scopes=SCOPES)
 
 spreadsheet_service = build('sheets', 'v4', credentials=credentials)
